6fb5dd489dc9d558b28258756e2035e2336b29ae
elrok123 <conner@systemicconfluence.com>
2017-08-09 16:03:13 +0200

    Removed TLD loops due to ridiculous overhead

--- src/scraper.cr
+++ src/scraper.cr
@@ -5,12 +5,12 @@ require "mongo"
 require "colorize"
 
 # Custom classes
-require "./classes/*"
+require "./classes/mongo.cr"
 
 
 class Combinator
 	@domains_collection : Mongo::Collection | Nil
-	def initialize(@tlds : Array(String))
+	def initialize()
 		@alphabet_arr = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
 		@mongo_factory = MongoController.new("127.0.0.1", "domainUser", "168971", "27017", "admin")
@@ -22,22 +22,20 @@ class Combinator
 			domain_permutations = [] of String
 			("a".."z").to_a.each_permutation(length) do |permutation|
 				domain = permutation.join
-				@tlds.each do |tld|
-					begin
-						puts "Attempting to add domain #{domain + tld}"
-						self.add_domain(domain + tld)
-					rescue 
-						attempts = 4
-						(0..attempts).each do |attempt|
-							puts "There was a problem adding #{domain + tld}, retrying... Attempt no. #{attempt+1}"
-							begin
-								self.add_domain(domain + tld)
-								puts "Successfully added #{domain + tld} after #{attempt+1} attempts..."
-							rescue
-								puts "Failed to add #{domain + tld} again... #{attempts - (attempt+1)} remaining..."
-							else
-								puts "Failed to add #{domain + tld} again... #{attempts - (attempt+1)} remaining..."
-							end
+				begin
+					puts "Attempting to add domain #{domain}"
+					self.add_domain(domain)
+				rescue 
+					attempts = 4
+					(0..attempts).each do |attempt|
+						puts "There was a problem adding #{domain}, retrying... Attempt no. #{attempt+1}"
+						begin
+							self.add_domain(domain)
+							puts "Successfully added #{domain} after #{attempt+1} attempts..."
+						rescue
+							puts "Failed to add #{domain} again... #{attempts - (attempt+1)} remaining..."
+						else
+							puts "Failed to add #{domain} again... #{attempts - (attempt+1)} remaining..."
 						end
 					end
 				end
@@ -54,9 +52,9 @@ class Combinator
 		
 	end
 end
-combiner = Combinator.new(TLDS)
+combiner = Combinator.new
 (1..1000).each do |string_length|
 	puts "Combining string length of #{string_length}..."
-	permutations = combiner.get_permutations(string_length)
+	combiner.get_permutations(string_length)
 	puts "Finished combining strings of length #{string_length}"
 end
